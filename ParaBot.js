99cf62fcc00626aa625ccc2496eb25df9ff6f43e
alex-taxiera <alex.taxiera@gmail.com>
2018-02-12 18:43:08 -0500

    move command handler to main file

--- ParaBot.js
+++ ParaBot.js
@@ -4,14 +4,13 @@ const colors = require('colors')
 const moment = require('moment')
 
 // project modules
-const config = require('./config.json')
-const { handleCommand } = require('./modules/commands.js')
+const { token, games, prefix } = require('./config.json')
+const commands = require('./modules/commands')
 const api = require('./modules/api.js')
 
 // connect bot
-const tok = config.token
-if (tok !== '') {
-  var bot = new Eris(tok)
+if (token !== '') {
+  var bot = new Eris(token)
   bot.connect()
 } else {
   log('no token', 'red')
@@ -30,20 +29,17 @@ bot.on('ready', () => {
   api.buildMaps()
 })
 
-bot.on('messageCreate', async (msg) => {
-  handleCommand(msg, config, bot)
-})
+bot.on('messageCreate', handleCommand)
 
 // helpers
 function setGame () {
-  let games = config.games
   let name = games[Math.floor(rng() * games.length)]
   log(`playing ${name}`, 'cyan')
   bot.editStatus('online', { name })
   setTimeout(setGame.bind(), 43200000) // 43200000
 }
 
-function log (str, color, err) {
+function log (str, color) {
   if (typeof str !== 'string') {
     str = str.toString()
   }
@@ -52,4 +48,43 @@ function log (str, color, err) {
       `BZZT ${str.toUpperCase()} BZZT`
     )
   )
+}
+
+async function handleCommand (msg) {
+  if (msg.member && msg.member.id !== bot.user.id && msg.content.startsWith(prefix)) {
+    msg.delete().catch(console.error) // delete invoking message if you can
+
+    let fullParam = msg.content.substring(prefix.length)
+    let params = fullParam.split(' ')
+    if (!isNaN(params[params.length - 1])) { var level = params.pop() }
+    let heroId = api.getHero(fullParam)
+    let cardId = api.getCard(fullParam)
+
+    if (heroId) {
+      response(msg, await commands['hero'].execute(heroId))
+    } else if (cardId) {
+      response(msg, await commands['card'].execute(cardId, level))
+    }
+
+    // else
+    let command = commands[params.splice(0, 1)[0]]
+    fullParam = params.join(' ')
+
+    if (command) {
+      if (command.name === 'hero' || command.name === 'card') {
+        response(msg, { response: `usage: '${prefix + fullParam}'` })
+      } else if (params.length < command.parameters.length) {
+        response(msg, { response: msg.author.mention + ' insufficient parameters' })
+      } else {
+        response(msg, await command.execute({ msg, params, fullParam, level, commands }))
+      }
+    }
+  }
+}
+
+async function response(msg, { response, delay = 10000 }) {
+  if (!response.embed) { response = `${msg.author.mention} ${response}` }
+  msg.channel.createMessage(response)
+    .then((m) => { setTimeout(() => { m.delete() }, delay) })
+    .catch(console.error)
 }
